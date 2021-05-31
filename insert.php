59396f486f0bd6b74ea790811dadc4d98e76d384
jhonatan <jhonatan.geremias>
2021-05-30 22:36:37 -0300

    first commit

--- insert.php
+++ insert.php
@@ -1,4 +1,3 @@
-
 <html>
 <body>
 
@@ -9,29 +8,5 @@ Country: <?php echo $_POST["country"]; ?><br>
 </body>
 </html> 
 
-<?php
-
-$nome = $_POST['name'];
-$occupation = $_POST['occupation'];
-$country = $_POST['country'];
-
-echo $nome
-echo $occupation
-echo $country
-
-// read json file
-$data = file_get_contents('data.json');
-
-// decode json
-$json_arr = json_decode($data, true);
-
-
-
-// add data
-$json_arr[] = array('name'=>$nome, 'occupation'=>$occupation, 'country'=>$country);
-
-// encode json and save to file
-file_put_contents('data.json', json_encode($json_arr));
-?>
 
 
