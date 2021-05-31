c77f2902ea7b1c28327ee21e537f5830d2465c6f
jhonatan <jhonatan.geremias>
2021-05-30 21:44:59 -0300

    first commit

---
+++ cadastrar.php
@@ -0,0 +1,12 @@
+<html>
+	<head> 
+	</head>
+	<body>
+		<form method="POST" action="insert.php">
+			<label>Nome:<label> <input type="text" name="name">
+			<label>Ocupação:<label> <input type="text" name="occupation">
+			<label>Pais:<label> <input type="text" name="country">
+			<input type="submit" value="Cadastrar">
+		</form>
+	</body>
+</html>
--- insert.php
+++ insert.php
@@ -1,13 +1,24 @@
 <?php
+
+$nome = filter_input(INPUT_POST,'name',FILTER_SANITINE_STRING);
+$occupation = filter_input(INPUT_POST,'occupation',FILTER_SANITINE_STRING);
+$country = filter_input(INPUT_POST,'country',FILTER_SANITINE_STRING);
+
+
+
 // read json file
 $data = file_get_contents('data.json');
 
 // decode json
 $json_arr = json_decode($data, true);
 
+
+
 // add data
-$json_arr[] = array('name'=>'Jhonatan', 'occupation'=>'Jeff Darwin', 'country'=>'Brasil');
+$json_arr[] = array('name'=>$nome, 'occupation'=>$occupation, 'country'=>$country);
 
 // encode json and save to file
 file_put_contents('data.json', json_encode($json_arr));
 ?>
+
+
