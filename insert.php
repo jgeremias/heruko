b3601c3dd8b1ab184d68e90975b97ba704055354
jhonatan <jhonatan.geremias>
2021-05-30 22:52:44 -0300

    first commit

--- insert.php
+++ insert.php
@@ -9,4 +9,17 @@ echo $b;
 echo $c;
 
 
+$data = file_get_contents('data.json');
+
+// decode json
+$json_arr = json_decode($data, true);
+
+
+// add data
+$json_arr[] = array('name'=>$a, 'occupation'=>$b, 'country'=>$c);
+
+// encode json and save to file
+file_put_contents('data.json', json_encode($json_arr));
+
+
 ?>
